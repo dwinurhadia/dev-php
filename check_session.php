f9c34649f44abe2ef7b515be48daa01fbe59cd58
Dwi Nurhadi Afriyanto <dwi.nurhadi17@student.uns.ac.id>
2022-06-24 18:30:29 +0700

    check session global function

--- check_session.php
+++ check_session.php
@@ -1,1 +1,25 @@
-<?php echo $_SERVER['PHP_SELF'];?>
+<!DOCTYPE html>
+<html>
+<body>
+
+<form method="post" action="<?php echo $_SERVER['PHP_SELF'];?>">
+  Name: <input type="text" name="fname">
+  <input type="submit">
+</form>
+
+<?php
+echo $_SERVER['PHP_SELF'];
+echo "<br>";
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // collect value of input field
+    $name = $_POST['fname'];
+    if (empty($name)) {
+        echo "Name is empty";
+    } else {
+        echo $name;
+    }
+}
+?>
+
+</body>
+</html>
